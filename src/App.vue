157c7dadb0795d50af9ba32029afde46ca0c8ef4
JERRY LI (ITA-ISDC-ISD-OOCLL/ZHA) <LIJE9@OOCL.COM>
2019-07-19 15:34:17 +0800

    [jerryLi] feat : finish ac1 add and sub number

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <p>{{counter}}</p>
+    <button @click="counter ++">点我+</button>
+    <button @click="counter --">点我-</button>
+    <button @click="extra">抽方法</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data:function(){
+    return {
+      counter : 0
+    }
+  },
+  methods:{
+    extra : function(){
+      this.counter == "抽方法" ? this.counter = 0 : this.counter = "抽方法";
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
